1ae097892a50b17f8a5561170c051c05b2983950
Karol <Karol@DESKTOP-IV6PM94>
2018-10-09 00:15:44 +0200

    first part of the app interface

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using MultiscaleModelingApp.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +22,85 @@ namespace MultiscaleModelingApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Grain[,] GrainTable { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void PaintPane()
+        {
+           
+            int numOfCells = int.Parse(XTextBox.Text);
+            int width = (int)MainCanvas.Width / numOfCells;
+            for (int i = 0; i < MainCanvas.Width; i += width)
+            {
+                Line line = new Line();
+                line.Stroke = Brushes.DarkBlue;
+
+                line.X1 = i;
+                line.X2 = i;
+                line.Y1 = 0;
+                line.Y2 = MainCanvas.Height;
+
+                line.StrokeThickness = 0.5f;
+                MainCanvas.Children.Add(line);
+            }
+            numOfCells = int.Parse(YTextBox.Text);
+            int height = (int)MainCanvas.Height / numOfCells;
+            for (int i = 0; i < MainCanvas.Height; i += height)
+            {
+                Line line = new Line();
+                line.Stroke = Brushes.DarkBlue;
+
+                line.X1 = 0;
+                line.X2 = MainCanvas.Width;
+                line.Y1 = i;
+                line.Y2 = i;
+
+                line.StrokeThickness = 0.5f;
+                MainCanvas.Children.Add(line);
+            }
+            GrainTable = new Grain[int.Parse(XTextBox.Text), int.Parse(YTextBox.Text)];
+            for (int i = 0; i < int.Parse(XTextBox.Text); i++)
+            {
+                for (int j = 0; j < int.Parse(YTextBox.Text); j++)
+                {
+                    GrainTable[i, j] = new Grain(0, int.Parse(XTextBox.Text) / (int)MainCanvas.Width, int.Parse(YTextBox.Text) / (int)MainCanvas.Height);
+                    Canvas.SetLeft(GrainTable[i, j].Rect, i*width);
+                    Canvas.SetTop(GrainTable[i, j].Rect, i*height);
+                    MainCanvas.Children.Add(GrainTable[i, j].Rect);
+                }
+            }
+        }
+
+        private void DrawPane(object sender, RoutedEventArgs e)
+        {
+
+            PaintPane();
+        }
+
+        private void SeedBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int numofSeeds = int.Parse(SeedCountTxtBox.Text);
+            Random r = new Random();
+            for (int i = 0; i < numofSeeds; i++)
+            {
+                GrainTable[r.Next(int.Parse(XTextBox.Text)), r.Next(int.Parse(YTextBox.Text))].Seed();
+            }
+            Thread t = new Thread(UpdateCanvas);
+            t.Start();
+        }
+        private void UpdateCanvas()
+        {
+            foreach(Grain g in GrainTable)
+            {
+                if (g!=null&&g.State == 1)
+                {
+                    int a = 0;
+                }
+            }
         }
     }
 }
---
+++ Model/Grain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace MultiscaleModelingApp.Model
+{
+    public class Grain
+    {
+        public Color Color { get; set; }
+        public int State { get; set; } = 0;
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Rectangle Rect{ get; set; }
+        public Grain(int state, int width, int height)
+        {
+            Color = Color.FromRgb(255, 255, 255);
+            Rect = new Rectangle();
+            Rect.Fill = new SolidColorBrush(Color);
+            Rect.Width = width;
+            Rect.Height = height;
+        }
+
+        public void Seed()
+        {
+            Random rand = new Random();
+            Color = Color.FromRgb(Convert.ToByte(rand.Next(255)), Convert.ToByte(rand.Next(255)), Convert.ToByte(rand.Next(255)));
+            State = 1;
+        }
+    }
+}
